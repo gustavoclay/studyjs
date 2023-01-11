2c6920d2bbd3dd2a42c62d042dfbe9c0b70dee42
gustavoclay <gustavoclay@gmail.com>
2023-01-11 10:17:04 -0300

    Nullish Coalescing Operator

---
+++ vanilla-js/vite-project/main.js
@@ -0,0 +1,11 @@
+// Nullish Coalescing Operator
+const idade = 0;
+
+// apenas null, undefined e etc
+console.log("🚀 ~ file: main.js:4 ~ idade", idade || 'Não informado')
+
+// considera valor real
+console.log("🚀 ~ file: main.js:4 ~ idade", idade ?? 'Não informado')
+
+
+
