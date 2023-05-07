b0790af550ca7e9b3bead159f97d34560a99d05e
gustavoclay <gustavoclay@gmail.com>
2023-05-07 01:19:26 -0300

    personalização do component com propriedades

--- react-next/app-inicial/src/components/basicos/Caixa.tsx
+++ react-next/app-inicial/src/components/basicos/Caixa.tsx
@@ -1,10 +1,11 @@
-export default function Caixa() {
+export default function Caixa(props: any) {
     return (
         <div className={`
+            flex justify-center items-center
             bg-purple-500 rounded-md p-2
-            w-64 h-64
+            w-64 h-64 - text-5xl font-thin
         `}>
-            Caixa
+            {props.children}
         </div>
     )
 }
