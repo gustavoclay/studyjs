e463ac45335c823559ee7423b1190f0592350c3f
Gustavo Clay <gustavoclay@gmail.com>
2023-08-29 01:26:30 -0300

    connect mysql and prisma

--- node/nestjs10-api/src/app.module.ts
+++ node/nestjs10-api/src/app.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CategoriesModule } from './categories/categories.module';
-import { CategoriesModule } from './categories/categories.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [CategoriesModule],
+  imports: [CategoriesModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
